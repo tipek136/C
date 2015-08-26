7937b4aad2c2c931e8752a4c15212bcf4b7f44c1
Uchyl_z_Vokurek <tipek136@gmail.com>
2015-08-26 13:05:37 +0200

    26.8.2015, 13:05 jo

---
+++ HelloWorld/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Tomáš
+ *
+ * Created on 26. srpna 2015, 13:01
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
